e4987af8d8ac04ef2ed7aacb6be186be59615dd8
Max Mills <8balldigitalsolutions@gmail.com>
2023-10-17 20:00:52 +0100

    init

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+
+ruby '3.1.0'
+group :development, :test do
+  gem 'byebug', '~> 9.0', '>= 9.0.6'
+  gem 'rspec'
+end
---
+++ lib/underground_system.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class UndergroundSystem
+  def initialize
+    @check_ins = {}
+    @journey_times = Hash.new { |hash, key| hash[key] = [0, 0] }
+  end
+
+  def check_in(id, station_name, time)
+    @check_ins[id] = [station_name, time]
+  end
+
+  def check_out(id, station_name, time)
+    start_station, start_time = @check_ins[id]
+    @journey_times[[start_station, station_name]][0] += time - start_time
+    @journey_times[[start_station, station_name]][1] += 1
+  end
+
+  def get_average_time(start_station, end_station)
+    total_time, total_trips = @journey_times[[start_station, end_station]]
+    # could use BigDecimal here to avoid rounding errors
+    total_time.to_f / total_trips
+  end
+end
