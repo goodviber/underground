6358be773deec2cdce8aa1413151bdb3b533779b
Max Mills <8balldigitalsolutions@gmail.com>
2023-10-18 09:24:45 +0100

    combine tests

--- spec/lib/underground_system_spec.rb
+++ spec/lib/underground_system_spec.rb
@@ -41,4 +41,37 @@ describe UndergroundSystem do
       expect(subject.get_average_time('Station B', 'Station A')).to be_nil
     end
   end
+
+  context 'with the first example description' do
+    it 'works' do
+      subject.check_in(45, 'Leyton', 3)
+      subject.check_in(32, 'Paradise', 8)
+      subject.check_out(45, 'Waterloo', 15)
+      subject.check_out(32, 'Cambridge', 22)
+      ans = subject.get_average_time('Paradise', 'Cambridge')
+      expect(ans).to eq 14
+      ans1 = subject.get_average_time('Leyton', 'Waterloo')
+      expect(ans1).to eq 12
+    end
+  end
+
+  context 'with the second example description' do
+    it 'works again' do
+      subject.check_in(10, 'Leyton', 3)
+      subject.check_in(45, 'Leyton', 3)
+      subject.check_in(32, 'Paradise', 8)
+      subject.check_in(27, 'Leyton', 10)
+      subject.check_out(45, 'Waterloo', 15)
+      subject.check_out(27, 'Waterloo', 20)
+      subject.check_out(32, 'Cambridge', 22)
+      ans = subject.get_average_time('Paradise', 'Cambridge')
+      expect(ans).to eq 14
+      ans1 = subject.get_average_time('Leyton', 'Waterloo')
+      expect(ans1).to eq 11
+      subject.check_in(10, 'Leyton', 24)
+      subject.check_out(10, 'Waterloo', 38)
+      ans2 = subject.get_average_time('Leyton', 'Waterloo')
+      expect(ans2).to eq 12
+    end
+  end
 end
